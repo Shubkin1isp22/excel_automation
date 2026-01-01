6f133f777e0d02c76ab7175a5755f4a94fae709f
Shubkin1isp22 <163159716+Shubkin1isp22@users.noreply.github.com>
2026-01-01 15:14:07 +0700

    fix logic

--- sql.py
+++ sql.py
@@ -24,29 +24,28 @@ df.columns = column_names_2
 filtered_data = dd[dd["Колонка 2"].isnull() | (dd["Колонка 2"].astype(str).str.strip() == "")].sort_values(by="Колонка 11", ascending=True)
 filteres_2list = df[df["Колонка 2"].isnull() | (df["Колонка 2"].astype(str).str.strip() == "")]
 
-
+last_table_11 = "keyy"
 for index_of_string, string in filtered_data.iterrows():
-    last_table_11 = "keyy"
     if string['Колонка 11'] != last_table_11:
         with open(f"./tables_sql/{string['Колонка 11']}.sql", "w", encoding="utf-8") as f:
+            
             # Дата-фрейм для каждой отдельной таблицы приёмника
             tgt_table_filter = filtered_data[filtered_data["Колонка 11"] == string['Колонка 11']]
             attr_data = tgt_table_filter
 
             # Атрибуты
-            attrs = "\n".join(
-                f"{string['Колонка 13']} {string['Колонка 14']} {string['Колонка 15'] if pd.notna(string['Колонка 15']) else "null"},"
+            attrs = "\n,".join(
+                f"{string['Колонка 13']} {string['Колонка 14']} {string['Колонка 15'] if pd.notna(string['Колонка 15']) else "null"}"
                 for _, string in attr_data.iterrows()
             )
             
             keys = "noup"
             date_1str = filteres_2list[filteres_2list['Колонка 6'] == str(string['Колонка 11'])]
-            if (string['Колонка 11'] == date_1str['Колонка 6'].iloc[0]) & (string['Колонка 3'] == date_1str['Колонка 3'].iloc[0]) & (str(string['Колонка 4']).strip() == str(date_1str['Колонка 4'].iloc[0]).strip()):
+            if (string['Колонка 3'] == date_1str['Колонка 3'].iloc[0]) & (str(string['Колонка 4']).strip() == str(date_1str['Колонка 4'].iloc[0]).strip()):
                 keys = f"{date_1str["Колонка 13"].iloc[0]}"
 
 
-            # Запись в txt
-            f.write(f"Строка {index_of_string + 1}\n")
+            # Запись в sql
             f.write(f"DROP TABLE if exists {string['Колонка 11']} cascade;\n\nCREATE TABLE {string['Колонка 11']} (\n{attrs}\n)\nWITH (\n\tappendonly=true,\n\torientation=column,\n\tcompresstype=zstd,\n\tcompresslevel=1\n)\nDISTRIBUTED BY ({keys});\n\n")
             last_table_11 = string['Колонка 11']
         
